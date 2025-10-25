9e22b2c5f0e3101be7f376ad7aae8db2477d906b
skillmates07-stack <skillmates07@gmail.com>
2025-10-25 20:42:44 +0530

    Update widgetRoutes.js

--- src/routes/widgetRoutes.js
+++ src/routes/widgetRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const auth = require('../middlewares/auth');
+const auth = require('../middleware/auth');
 const Widget = require('../models/Widget');
 
 /**
